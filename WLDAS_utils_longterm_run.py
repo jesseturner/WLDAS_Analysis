0256ff425c0f4a27afe6473900b035b9064bc95d
Jesse Turner <jturner@polarbear3.cira.colostate.edu>
2025-07-29 18:47:44 +0000

    Nohup run for creating the histograms

---
+++ WLDAS_utils_longterm_run.py
@@ -0,0 +1,17 @@
+from WLDAS_utils import WldasData
+from datetime import datetime, timedelta
+
+start_date = datetime(2001, 1, 1)
+end_date = datetime(2001, 12, 31)
+
+current_date = start_date
+
+while current_date <= end_date:
+    wldas = WldasData(current_date, chunks={"lat": 300, "lon": 300})
+    wldas.filter_by_bounds(bounds=[27.5,44,-128,-100])
+
+    wldas.create_hist_for_variables(hist_name="all_data")
+    wldas.create_hist_for_variables(hist_name="dust_points")
+    
+    current_date += timedelta(days=1)
+
