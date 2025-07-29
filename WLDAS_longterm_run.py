bd0d81da056a59dc270f06ec1005c93793925d8e
Jesse Turner <jturner@polarbear3.cira.colostate.edu>
2025-07-29 20:33:09 +0000

    Added file delete (large WLDAS files)

--- WLDAS_longterm_run.py
+++ WLDAS_longterm_run.py
@@ -1,7 +1,8 @@
 from WLDAS_utils import WldasData
 from datetime import datetime, timedelta
+import os
 
-start_date = datetime(2001, 1, 1)
+start_date = datetime(2001, 2, 1)
 end_date = datetime(2001, 12, 31)
 
 current_date = start_date
@@ -15,6 +16,13 @@ while current_date <= end_date:
 
         wldas.create_hist_for_variables(hist_name="all_data")
         wldas.create_hist_for_variables(hist_name="dust_points")
+
+        #--- Delete WLDAS data
+        file_path = f"WLDAS_data/WLDAS_NOAHMP001_DA1_{current_date.strftime('%Y%m%d')}.D10.nc"
+        if os.path.exists(file_path):
+            os.remove(file_path)
+        else:
+            print("File not found.")
     
     current_date += timedelta(days=1)
 
