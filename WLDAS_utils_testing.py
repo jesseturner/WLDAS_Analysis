9edf7f91116ad96f81d0732a91263ee2fdd9b535
Jesse Turner <jturner@polarbear3.cira.colostate.edu>
2025-07-31 17:58:14 +0000

    Testing histograms

--- WLDAS_utils_testing.py
+++ WLDAS_utils_testing.py
@@ -1,8 +1,9 @@
 from WLDAS_utils import WldasData
 from datetime import datetime
+import glob, os
 
 #--- Set up with values from (2002, 1, 2)
-wldas = WldasData(datetime(2002, 1, 2), chunks={"lat": 300, "lon": 300}, print_vars=False)
+wldas = WldasData(datetime(2002, 1, 2), chunks={"lat": 300, "lon": 300}, print_vars=False, print_ds=False)
 
 #--- __init__
 assert wldas.is_file_loaded(), "FAIL: File not loaded in __init__"
@@ -14,11 +15,16 @@ wldas.filter_by_bounds(bounds=[27.5,44,-128,-100])
 assert wldas.get_shape() == (1, 1650, 2493)
 print("SUCCESS: filter_by_bounds")
 
+#--- create_hist_for_variables
+wldas.create_hist_for_variables(hist_name="test")
+assert os.path.isfile("WLDAS_hist/test_20020102.pkl")
+print("SUCCESS: create_hist_for_variables")
 
+#--- plot_hist_for_variables
+wldas.plot_hist_for_variables(hist_name="test")
+assert len(glob.glob("WLDAS_hist_plots/test*")) == 41
+print("SUCCESS: plot_hist_for_variables")
 
-#wldas.create_hist_for_variables(hist_name="all_data")
-#wldas.create_hist_for_variables(hist_name="dust_points")
-
-
-#wldas.plot_hist_for_variables(hist_name="dust_points")
-
+#--- delete created files
+for file_path in glob.glob("WLDAS_hist_plots/test*"):
+    os.remove(file_path)
--- z_WLDAS_longterm_run.py
+++ z_WLDAS_longterm_run.py
@@ -1,6 +1,5 @@
 from WLDAS_utils import WldasData
 from datetime import datetime, timedelta
-import os
 
 start_date = datetime(2001, 2, 1)
 end_date = datetime(2001, 12, 31)
@@ -10,7 +9,7 @@ current_date = start_date
 while current_date <= end_date:
     wldas = WldasData(current_date, chunks={"lat": 300, "lon": 300})
     
-    if wldas.is_loaded():
+    if wldas.is_file_loaded():
         wldas.filter_by_bounds(bounds=[27.5,44,-128,-100])
 
         wldas.create_hist_for_variables(hist_name="all_data")
