24b2f55b0b56e496727e7c9fbeba4a905cc2d770
Jesse Turner <jturner@polarbear3.cira.colostate.edu>
2025-08-05 22:50:57 +0000

    Modularizing the functions further

--- WLDAS_utils/wldas_utils.py
+++ WLDAS_utils/wldas_utils.py
@@ -7,7 +7,7 @@ def get_wldas_data(date, chunks=None, print_vars=False, print_ds=False):
         download_dir = Path("WLDAS_data")
         filepath = _get_local_wldas(date, download_dir)
         if not filepath:
-            filepath = _download_wldas(date, download_dir)
+            filepath = _run_download_wldas(date, download_dir)
         if filepath:
             ds = _load_data_with_xarray(filepath, chunks, print_vars, print_ds)
         else: 
@@ -19,16 +19,13 @@ def get_wldas_data(date, chunks=None, print_vars=False, print_ds=False):
 def _get_local_wldas(date, download_dir):
     date_str = date.strftime("%Y%m%d")
     matches = list(download_dir.glob(f"*{date_str}*"))
-    filepath = None
     if matches:
         filepath = str(matches[0])
         print(f"Found file: {filepath}")
-    else:
-        print(f"Running download to get file.")
-        print("Warning: WLDAS data files are large, each ~900 MB.")
+    else: filepath = None
     return filepath
 
-def _download_wldas(date, download_dir):
+def _run_download_wldas(date, download_dir):
     #--- Set .netrc with GES DISC username and password
     #--- Add a reminder if there is a "no permissions" error
 
@@ -45,33 +42,19 @@ def _download_wldas(date, download_dir):
     #--- Make download directory
     download_dir.mkdir(parents=True, exist_ok=True)
 
-    #--- Download file
+    filepath = _download_wldas(session, url, download_dir)
+    return filepath
+
+def _download_wldas(session, url, download_dir):
     print(f"Connecting to {url}...")
     response = session.get(url, stream=True)
     print("Connection established. Starting download...")
 
     if response.status_code == 200:
-        #--- Extract filename from content-disposition if present
-        cd = response.headers.get('content-disposition')
-        if cd and 'filename=' in cd:
-            filename = cd.split('filename=')[-1].strip('\"')
-        else:
-            filename = url.split('/')[-1]
 
+        filename = _extract_filename(response, url)
         filepath = download_dir / filename
-
-        #--- Write file to local disk
-        total_size = int(response.headers.get('content-length', 0))
-        chunk_size = 8192
-
-        use_tqdm = sys.stderr.isatty() # Only use progress bar for in commandline
-        with open(filepath, 'wb') as f, tqdm(
-            total=total_size, unit='B', unit_scale=True, desc=filename, disable=not use_tqdm
-        ) as pbar:
-            for chunk in response.iter_content(chunk_size=chunk_size):
-                if chunk:
-                    f.write(chunk)
-                    pbar.update(len(chunk))
+        _write_file_to_local_disk(response, filepath, filename)
 
         print(f"Downloaded to {filepath}")
     else:
@@ -79,6 +62,28 @@ def _download_wldas(date, download_dir):
         print(response.text)
 
     return filepath
+
+def _extract_filename(response, url):
+    cd = response.headers.get('content-disposition')
+    if cd and 'filename=' in cd:
+        filename = cd.split('filename=')[-1].strip('\"')
+    else:
+        filename = url.split('/')[-1]
+    return filename
+    
+def _write_file_to_local_disk(response, filepath, filename):
+    total_size = int(response.headers.get('content-length', 0))
+    chunk_size = 8192
+
+    use_tqdm = sys.stderr.isatty() # Only use progress bar for in commandline
+    with open(filepath, 'wb') as f, tqdm(
+        total=total_size, unit='B', unit_scale=True, desc=filename, disable=not use_tqdm
+    ) as pbar:
+        for chunk in response.iter_content(chunk_size=chunk_size):
+            if chunk:
+                f.write(chunk)
+                pbar.update(len(chunk))
+    return
 
 def _load_data_with_xarray(filepath, chunks, print_vars, print_ds):
         if filepath:
--- main.py
+++ main.py
@@ -2,3 +2,4 @@ from WLDAS_utils import wldas_utils as wldas
 from datetime import datetime
 
 ds = wldas.get_wldas_data(datetime(2003, 1, 9), chunks={"lat": 300, "lon": 300}, print_vars=True, print_ds=True)
+
