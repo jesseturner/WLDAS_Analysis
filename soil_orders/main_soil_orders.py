7b6ce60b7dfe3873373abbffc1593d652cd4c379
Jesse Turner <jturner@polarbear3.cira.colostate.edu>
2025-10-04 01:09:20 +0000

    Loading shapefiles with geopandas

--- soil_orders/main_soil_orders.py
+++ soil_orders/main_soil_orders.py
@@ -1,5 +1,6 @@
 import soil_orders_utils as soil_orders
+import os
 
-wrb2014_file_dir = "WRB2014_soil_map"
 plot_dir = "/home/jturner/WLDAS_Analysis/soil_orders"
-soil_orders.open_wrb2014_file(wrb2014_file_dir, plot_dir, "example_wrb2014_plot")
+wrb2014_file_dir = os.path.join(plot_dir, "WRB2014_soil_map")
+soil_orders.open_wrb2014_file(wrb2014_file_dir, plot_dir, os.path.join(plot_dir, "example_wrb2014_plot"))
--- soil_orders/soil_orders_utils.py
+++ soil_orders/soil_orders_utils.py
@@ -1,5 +1,6 @@
 import geopandas as gpd
 import os, glob
+import pandas as pd
 import matplotlib.pyplot as plt
 
 def open_wrb2014_file(wrb2014_file_dir, plot_dir, plot_path):
@@ -7,11 +8,16 @@ def open_wrb2014_file(wrb2014_file_dir, plot_dir, plot_path):
     layers = [gpd.read_file(shp) for shp in shapefiles]
     print(f"Loaded {len(layers)} shapefiles")
 
-    fig, ax = plt.subplots(figsize=(10, 10))
-    for gdf in layers:
-        gdf.plot(ax=ax)
-    _plot_save(fig, plot_dir, plot_path)    
-    return
+    gdf = gpd.GeoDataFrame(pd.concat(layers, ignore_index=True))
+    print(gdf)
+
+    return gdf
+
+    # fig, ax = plt.subplots(figsize=(10, 10))
+    # for gdf in layers:
+    #     gdf.plot(ax=ax)
+    # _plot_save(fig, plot_dir, plot_path)    
+    # return
 
 def _plot_save(fig, plot_dir, plot_path):
     plt.tight_layout()
