feebfd917dfa3990ee69ab3978c6c309fc122e21
Jesse Turner <jturner@polarbear3.cira.colostate.edu>
2025-12-16 19:09:07 +0000

    Reinstating main scripts

--- main_soil_moisture.py
+++ main_soil_moisture.py
@@ -2,10 +2,30 @@ from modules_soil_moisture import wldas_utils as moist
 from modules_line_dust import line_dust_utils as dust
 
 #--- open soil moisture subset files
-moist.create_moist_histogram(dir_path="/mnt/data2/jturner/wldas_data/")
+wldas_path = "/mnt/data2/jturner/wldas_data/"
+moist.create_moist_histogram(dir_path=wldas_path)
 
 #--- Relate WLDAS soil moisture to dust data
-dust_path="line_dust_data/dust_dataset_final_20241226.txt"
+dust_path="data/raw/line_dust/dust_dataset_final_20241226.txt"
 dust_df = dust.read_dust_data_into_df(dust_path)
 dust_region_df = dust.filter_to_region(dust_df, location_name="American Southwest")
-# print(dust_region_df)
+# ds = wldas.filter_by_dust_points(ds, dust_df)
+# ds = wldas.create_hist_for_variables(ds, "WLDAS_hist_test")
+# ds = wldas.plot_hist_for_variables(ds, "WLDAS_hist_test")
+
+#--- WLDAS soil moisture time series before and after dust events
+# plus_minus_30_dir = "data/processed/WLDAS_plus_minus_30"
+# filepath = f"{wldas_path}/WLDAS_NOAHMP001_DA1_20010112.D10.nc.SUB.nc4"
+# ds = wldas.load_data_with_xarray(filepath, chunks=None, print_vars=False, print_ds=True)
+# wldas.get_wldas_plus_minus_30(dust_df, wldas_path, plus_minus_30_dir)
+# json_filepath = "data/processed/WLDAS_plus_minus_30/20021217_1845_lat3041_lon10653.json"
+# wldas.plot_wldas_plus_minus_30(json_filepath, "WLDAS_plus_minus_30_plots")
+
+#--- Tracking WLDAS moisture patterns (total, yearly, frequency)
+# fig_dir = "figures"
+# save_dir = "data/processed/wldas_time_pattern"
+# wldas.create_region_average_over_time(wldas_dir=wldas_path, location_name="Chihuahua", save_dir=save_dir)
+# csv_path = f"{save_dir}/region_moisture_Chihuahua.csv"
+# wldas.plot_region_average_over_time(csv_path, plot_dir=fig_dir, location_str="Chihuahua")
+# wldas.plot_region_average_over_year(csv_path, dust_region_df=dust_region_df, plot_dir=fig_dir, location_str="Chihuahua")
+# wldas.plot_frequency_analysis(csv_path, dust_region_df=dust_region_df, plot_dir=fig_dir, location_str="Chihuahua")
---
+++ main_textures.py
@@ -0,0 +1,22 @@
+from modules_texture import gldas_texture_utils as gldas
+from modules_line_dust import line_dust_utils as dust
+
+#--- Open GLDAS soil textures (percentages)
+gldas_path = "data/raw/gldas_soil_texture/GLDASp4_soilfraction_025d.nc4"
+location_name = "American Southwest"
+texture_ds = gldas.open_gldas_file(gldas_path)
+texture_ds = gldas.filter_to_region(texture_ds, location_name)
+print(texture_ds)
+texture_average_df = gldas.get_texture_averages_for_region(texture_ds)
+print(texture_average_df)
+
+#--- Plot soil texture for each dust event
+# dust_path = "Line_dust_data/dust_dataset_final_20241226.txt"
+# dust_df = dust.read_dust_data_into_df(dust_path)
+# dust_region_df = dust.filter_to_region(dust_df, location_name=location_name)
+# texture_fractions_df = gldas.get_texture_for_dust_events(texture_ds, dust_df)
+# print(texture_fractions_df)
+# gldas.create_ternary_plot(texture_fractions_df, fig_dir="figures", 
+#     fig_name=f"ternary_{location_name.lower().replace(" ", "_")}_dust_producing", 
+#     fig_title=f"Distribution of dust-producing points: {location_name}",
+#     texture_average_df=texture_average_df)
