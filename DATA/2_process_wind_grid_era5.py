Fixing combo bar chart, which still had nulls in total calculation

DATA/2_process_wind_grid_era5.py
@@ -11,20 +11,20 @@ import time
 def main(): 
     start = time.time()
 
-    with Client(dashboard_address="127.0.0.1:8787") as client:
-        print(client)
+    # with Client(dashboard_address="127.0.0.1:8787") as client:
+        #print(client)
 
-        ds_ws = get_wind_speeds()
+    get_wind_speeds()
 
-        ds_daytime_max = get_daytime_max_ws(ds_ws)
+        # ds_daytime_max = get_daytime_max_ws(ds_ws)
         
-        ds_daytime_max = crop_to_region_and_land(ds_daytime_max)
+        # ds_daytime_max = crop_to_region_and_land(ds_daytime_max)
 
-        print("Saving to netcdf...")
-        timestamp = datetime.today().strftime("%Y-%m-%d")
-        ds_daytime_max = ds_daytime_max.chunk({"x": 90, "y": 65, "time": 100})
-        print(ds_daytime_max.chunks)
-        ds_daytime_max.to_netcdf(f"DATA/processed/2_wind_grid_{timestamp}.nc")
+        # print("Saving to netcdf...")
+        # timestamp = datetime.today().strftime("%Y-%m-%d")
+        # ds_daytime_max = ds_daytime_max.chunk({"x": 90, "y": 65, "time": 100})
+        # print(ds_daytime_max.chunks)
+        # ds_daytime_max.to_netcdf(f"DATA/processed/2_wind_grid_era5_{timestamp}.nc")
         
     end = time.time()
     print(f"Time to process: {end - start:.2f} seconds")
@@ -34,17 +34,22 @@ def main():
 #------------------------
 
 def get_wind_speeds():   
-    print("Opening data from NARR...")
-    ds_uwnd = xr.open_mfdataset("/mnt/data2/jturner/narr/uwnd.10m.20*.nc",
+    print("Opening data from ERA-5...")
+    ds_era5 = xr.open_mfdataset("/mnt/data2/jturner/wind_era5/*.nc",
                                 chunks="auto")
-    ds_vwnd = xr.open_mfdataset("/mnt/data2/jturner/narr/vwnd.10m.20*.nc",
-                                chunks="auto")
+    
+    print(ds_era5)
 
-    print("Calculating wind speed...")
-    wind_speed = np.sqrt(ds_uwnd.uwnd**2 + ds_vwnd.vwnd**2)
-    ds_ws = wind_speed.to_dataset(name="wind_speed")
+    unique_years = ds_era5["valid_time"].dt.year.values
+    unique_years = sorted(set(unique_years))
+    print(unique_years)
 
-    return ds_ws
+
+    # print("Calculating wind speed...")
+    # wind_speed = np.sqrt(ds_uwnd.uwnd**2 + ds_vwnd.vwnd**2)
+    # ds_ws = wind_speed.to_dataset(name="wind_speed")
+
+    return 
 
 def get_daytime_max_ws(ds_ws):
 